dcbb8a0943c74ed9decd4a189a75f35e5a11b372
falilah <leogoldf21@gmail.com>
2022-06-19 11:59:59 +0100

    smart contracts

--- backend/src/governance.sol
+++ backend/src/governance.sol
@@ -2,39 +2,35 @@
 pragma solidity 0.8.10;
 import "./funderGovToken.sol";
 
-/*
+  /*
   * @title Funder logic for raising funds .
   * @notice  web3 version of crowdFundMe.
   */
 
 contract Voting is FunderGovToken{
-     /*******************sTATE VARIABLES************************/
+    /*******************sTATE VARIABLES************************/
      uint aYesVote;
-     uint MINIMUMAPPROVAL = 10;
+     uint MINIMUMAPPROVAL = 20;
 
 
-  /*******************MAPPING************************/
+    /*******************MAPPING************************/
     mapping (address => mapping(address => bool))public govVote;
     mapping (address => mapping(address => uint))public govVoteCount;
     mapping (address => bool) public RequestGranted;
 
-     /*******************EVENTS************************/  
+    /*******************EVENTS************************/  
      event Voted(address indexed gov, address indexed ben);
      event unlock(address indexed owner, uint indexed _amount);
 
-
-
-
-     /*******************MODIFIER************************/  
+    /*******************MODIFIER************************/  
       modifier checkUnlockToken(address owner, uint amount){
       uint valueLocked = lockedToken[msg.sender];
       uint bal = balanceOf(owner);
       require(valueLocked + amount < bal, "you don't have token to vote");
       _;
-  }
-
-
-     /*******************CONSTRUCTOR************************/  
+    }
+     
+    /*******************CONSTRUCTOR************************/  
      constructor(uint _aYesVote) FunderGovToken("MYFunder","MYF"){
         aYesVote = _aYesVote;
         _mint(msg.sender, 1000000e18);
@@ -42,9 +38,13 @@ contract Voting is FunderGovToken{
 
      }
 
-     /******************* Govenance FUNCTIONS************************/  
-         
-     function voteProposal(address castVote, uint amount) external checkUnlockToken(msg.sender, amount) {
+    /******************* Govenance FUNCTIONS************************/  
+     /**
+     * @notice vote for the beneficiary that requested for fund
+     * @param castVote the beneficiary address
+     * @param amount the amount of token to use in vote
+     */
+    function voteProposal(address castVote, uint amount) external checkUnlockToken(msg.sender, amount) {
          require(amount  % aYesVote == 0, "10 tokens is 1 yes");
          assert(!(govVote[msg.sender][castVote]));
          uint noOfVote = amount / aYesVote;
@@ -54,33 +54,42 @@ contract Voting is FunderGovToken{
             RequestGranted[castVote] = true;   
          }
          emit Voted(msg.sender, castVote);
-     }
+    }
 
-
-     function lockToken(uint _amount, address castVote) private {
+     /**
+     * @notice lock the token that has been used to vote for a beneficiary
+     * @param castVote the beneficiary address
+     * @param _amount the amount of token to lock
+     */
+    function lockToken(uint _amount, address castVote) private {
       tokenOwners[msg.sender][castVote]+= _amount;
       lockedToken[msg.sender] += _amount;
-  }
-  function unLockToken(address castVote) private returns(uint){
+    }
+
+     /**
+     * @notice Unlock the token that has been used to vote for a beneficiary
+     * @param castVote the beneficiary address
+     */
+    function unLockToken(address castVote) external returns(uint){
+      require(govVoteCount[msg.sender][castVote] >= MINIMUMAPPROVAL, "This voting is still on going");
       uint256 value =  tokenOwners[msg.sender][castVote];
       lockedToken[msg.sender] -= value;
       tokenOwners[msg.sender][castVote]-= value;
+      emit unlock(msg.sender, value);
       return value;
 
-  }
+    }
 
-      function unLocked( address castVote) external{
-          require(govVoteCount[msg.sender][castVote] >= MINIMUMAPPROVAL, "This voting is still on going");
-          uint value = unLockToken(castVote);
-          emit unlock(msg.sender, value);
-      
-  }
+    /*******************VIEW FUNCTIONS************************/
+     /**
+     * @notice check the status of voting count for a beneficiaryVote
+     * @return status return the status of the vote if it has passed or not
+     **/
 
-  /*******************VIEW FUNCTIONS************************/
-  function getBeneficiaryVotingStatus(address beneficiary) public view returns(bool status){
-    status = RequestGranted[beneficiary];
+    function getBeneficiaryVotingStatus(address beneficiary) public view returns(bool status){
+      status = RequestGranted[beneficiary];
 
-  }
+    }
 
  
 
